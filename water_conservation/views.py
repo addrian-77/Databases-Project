41b131f43bda0c5626dcb85b5f55edc5273ab541
Adrian Lungu <lunguadrian30@gmail.com>
2025-01-04 13:25:54 +0200

    update

--- water_conservation/views.py
+++ water_conservation/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
-
 def index(request):
+    if request.method == 'POST':
+        greeting = request.POST.get('say', 'Hi')
+        recipient = request.POST.get('to', 'World')
+        if greeting and recipient:
+            result = f"You entered: {greeting}, {recipient}"
+        else:
+            result = ""
+        return render(request, 'homepage.html', {'result': result})
     return render(request, 'homepage.html')
