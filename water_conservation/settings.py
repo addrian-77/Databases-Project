422832b4e35eba423e615e3d3f80cc50b3932963
Adrian Lungu <lunguadrian30@gmail.com>
2025-01-04 13:12:12 +0200

    working HTML and CSS

--- water_conservation/settings.py
+++ water_conservation/settings.py
@@ -28,6 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 
 # Application definition
 
