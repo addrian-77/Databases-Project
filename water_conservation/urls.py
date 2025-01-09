2bf06bb802f16843f6a747c21b947dc2213f057a
Adrian Lungu <lunguadrian30@gmail.com>
2025-01-09 14:27:44 +0200

    watertips fixes

--- water_conservation/urls.py
+++ water_conservation/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage_view, name='index'),  # Homepage
+    path('', views.homepage_view, name='homepage'),  # Homepage
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
